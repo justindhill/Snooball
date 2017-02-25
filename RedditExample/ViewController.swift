e8f50161816f7f803151bab15b3b07c2908aa552
Justin Hill <jhill.d@gmail.com>
2017-02-25 13:05:49 -0800

    Initial Commit

---
+++ RedditExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditExample
+//
+//  Created by Justin Hill on 2/25/17.
+//  Copyright © 2017 Justin Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
